e0000883d138de833d47669f13087c751af81a8a
Bo-wei Chen <rayologist1002@gmail.com>
2022-02-19 01:36:16 +0800

    Add conditionals for production

--- src/api/v1/models/pgCli.js
+++ src/api/v1/models/pgCli.js
@@ -1,9 +1,22 @@
 import PG from "pg";
 
-const SHORTENER = new PG.Pool({
-    host: 'localhost',
-    database: 'shortener',
+export const generateConfig = () => {
+  if (process.env.NODE_ENV === "production") {
+    return {
+      host: "postgres",
+      user: "shortener",
+      database: "shortener",
+      password: "secret",
+      port: 5432,
+    };
+  }
+  return {
+    host: "localhost",
+    database: "shortener",
     port: 5432,
-});
+  };
+};
+
+const SHORTENER = new PG.Pool(generateConfig());
 
 export default SHORTENER;
--- src/api/v1/models/redisCli.js
+++ src/api/v1/models/redisCli.js
@@ -1,4 +1,12 @@
 import redis from "redis";
-const REDIS = redis.createClient();
+
+export const generateConfig = () => {
+  if (process.env.NODE_ENV === "production") {
+    return { url: "redis://redis:6379" };
+  }
+  return null;
+};
+
+const REDIS = redis.createClient(generateConfig());
 await REDIS.connect();
 export default REDIS;
